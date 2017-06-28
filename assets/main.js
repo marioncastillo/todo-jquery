558e0e32c67c743cc6719af6fb9c9e3f5c7eef03
MarionFoto <marionacp@gmail.com>
2017-06-28 15:45:49 -0400

    agrega tareas al click

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-	$("#tarea").keyup(function(e){
+	$("input").click(function(e){
 		e.preventDefault();
 		var tarea = $("#tarea").val();
-		if(tarea == ""){
-			alert("Ingresa tu tarea");
-		}else{
-			$('#check').append('<p><input type="checkbox">' + tarea + '</p>');
-		}
+
+	$('#check').append('<p><input type="checkbox">' + tarea + '</p>');
+	
 	});
 });
