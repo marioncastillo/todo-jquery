d07be2b66cd72d155003e2fdb534cf64e75d9cc3
MarionFoto <marionacp@gmail.com>
2017-06-29 10:12:20 -0400

    cargando css

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-	$("input").click(function(e){
+	$("input").click(function(e){ //agrega tarea al click
 		e.preventDefault();
 		var tarea = $("#tarea").val();
 
-	$('#check').append('<p><input type="checkbox">' + tarea + '</p>');
+	$('#check').append('<div class="cajatarea"><input type="checkbox" id="otra"/>' + tarea + '</label><button class="boton">Remove</button></div>');
 	
 	});
+
+
+
 });
